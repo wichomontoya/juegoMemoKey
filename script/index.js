67a16d2b5726c0cddbd55668f97c5f17a598a764
Javier Banguero <javierbanguero@hotmail.com>
2016-07-08 20:46:14 -0500

    Juego al 90%

--- script/index.js
+++ script/index.js
@@ -1,14 +1,20 @@
 
 $(document).ready(function(){
+
 	var alfabeto = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 	var letrasPc = [];
 	var letrasUsuario = [];
 	var turno = 0;
 	var puntos = 0;
 	var stop=false;
+	var contadorSegundos =60;
 
+	// Ejecucion del codigo
+
+	cronometro();	
 	generarLetra();
 
+	// Boton llama Funciones 
 			
 
 	$("#botonCapturaLetraUsuario").click(function(){
@@ -18,6 +24,8 @@ $(document).ready(function(){
 			generarLetra();
 		}
 	});
+
+	// Funcion Captura y divide las letras
 
 
 	function capturarLetra(){
@@ -35,25 +43,15 @@ $(document).ready(function(){
 
 	function generarLetra() {
 		
-		if(turno < 5){
 			var aleatorio = Math.round(Math.random()* 26);
 			var miLetra = alfabeto[aleatorio]
-			alert(miLetra);
+			letras = document.getElementById("letras");
+			letras.innerHTML = miLetra;
 			letrasPc.push(miLetra);
-			console.log(letrasPc);
-			turno=turno+1;
-		}
-		else{
-			alert("El Juego ha terminado, su puntaje es: " + puntos);
-		}
+			// console.log(letrasPc);
 	}
 
-
-
-
-
-
-	// Funcion que compara
+	// Funcion que compara arreglos
 
 	function comparaLetras(){
 
@@ -68,18 +66,36 @@ $(document).ready(function(){
 			else if (letrasPc[i]!==letrasUsuario[i]){ 
 				puntos=puntos-1;
 				alert("Te equivocaste");
-
-				// $("#mensaje").show(function(){
-
-				// });
 				letrasUsuario=[];
 				$('#letrausuario').val();
 				i=letrasPc.length;
 				stop=true
 			}
 		}
+
 		letrasUsuario=[];
 	}
+
+	function cronometro(){
+			
+		 s = document.getElementById("segundos");
+
+		var myVar= setInterval(
+
+			function(){
+				contadorSegundos=contadorSegundos-1;
+				s.innerHTML = contadorSegundos;
+				if (contadorSegundos<=0){
+					stop=true;
+					alert("El Juego ha terminado, su puntaje es: " +puntos);
+					clearInterval(myVar);
+				} 
+
+			},1000
+		);	
+
+	}
+
 });	
 
 
